3993091c08c9f8dc4559cbbc9a78aad6eb5cda71
mishamip <mishamip@gmail.com>
2025-08-25 21:34:05 +0300

    fix: обновлены зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ android {
 
 dependencies {
     // AndroidX Core
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
@@ -75,8 +75,8 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     
     // Hilt для WorkManager
-    implementation("androidx.hilt:hilt-work:1.1.0")
-    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.hilt:hilt-work:1.2.0")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
     
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
@@ -100,8 +100,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     
     // Hilt для внедрения зависимостей
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     
     // Timber для логирования
     implementation("com.jakewharton.timber:timber:5.0.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.library") version "8.12.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
 
 // Задача для очистки проекта
