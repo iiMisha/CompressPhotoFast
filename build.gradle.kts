e37dc8be56212aa52bd4f7fdb50fd3d104af7948
mishamip <mishamip@gmail.com>
2026-01-10 11:48:26 +0300

    fix(deps): обновлены версии Android Gradle Plugin до 8.13.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Корневой build файл проекта
 plugins {
-    id("com.android.application") version "8.13.1" apply false
-    id("com.android.library") version "8.13.1" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("com.android.library") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.jvm") version "2.2.10" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
