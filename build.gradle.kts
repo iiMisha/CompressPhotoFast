57b2fb8ea5d5060dd671c54f5a94e9ed9136c31d
mishamip <mishamip@gmail.com>
2026-02-15 09:52:45 +0300

    build(deps): обновлены версии Android Gradle плагинов до 9.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Корневой build файл проекта
 plugins {
-    id("com.android.application") version "9.0.0" apply false
-    id("com.android.library") version "9.0.0" apply false
+    id("com.android.application") version "9.0.1" apply false
+    id("com.android.library") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
     id("com.google.devtools.ksp") version "2.3.2" apply false
